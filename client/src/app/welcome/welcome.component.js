5ae569a28b4c796665d074ef1efe64b057888bdc
draffensperger <d.raffensperger@gmail.com>
2016-08-16 18:39:13 -0400

    Style and layout fixes

---
+++ client/src/app/welcome/welcome.component.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+  .module('wizweekPy')
+  .component('wwWelcome', {
+    controller: WelcomeController,
+    controllerAs: 'welcome',
+    templateUrl: '/app/welcome/welcome.html'
+  });
+
+  /** @ngInject */
+  function WelcomeController(auth) {
+    var vm = this;
+    vm.auth = auth;
+  }
+})();
